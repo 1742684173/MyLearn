29c3087f9b779a1ac3fcf35c0c2513e468b1f3ed
zxl <1742684173@qq.com>
2020-02-24 21:29:29 +0800

    Initial commit

---
+++ app/src/main/java/com/aloogn/mylearn/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aloogn.mylearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
